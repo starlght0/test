703698770550841c130994ce5f05c41a98ae4cfa
starlght0 <112991120+starlght0@users.noreply.github.com>
2022-11-15 23:16:41 +0700

    Create task5.cpp

---
+++ task5.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+void output(double otvet) {
+    cout << otvet;
+}
+
+void solution(double summa, double first_pay, double crok) {
+
+    double otvet = (summa - first_pay) / (11 + (crok - 1) * 12);
+
+    output(otvet);
+}
+
+void input() {
+
+    double summa, first_pay, crok;
+    cin >> summa >> first_pay >> crok;
+    if (first_pay < summa * 0.2) {
+        return;
+    }
+    solution(summa, first_pay, crok);
+}
+
+int main() {
+    input();
+    return 0;
+}
