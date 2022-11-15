f09a3df9af6a7bc6e4b522579a65097771e85eba
starlght0 <112991120+starlght0@users.noreply.github.com>
2022-11-15 23:10:23 +0700

    Create task2.cpp

---
+++ task2.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+using namespace std;
+
+void output(string otvet){
+    cout<<otvet;
+}
+
+void solution(int x, int y){
+
+    string otvet;
+
+    if(x>0 && y>0){
+        otvet = "I";
+    } if(x<0 && y>0){
+        otvet = "II";
+    } if(x<0 && y<0){
+       otvet = "III"; 
+    } if(x>0 && y<0){
+        otvet = "IV";
+    }
+
+    output(otvet);
+}
+
+void input(){
+    int x, y;
+    cin>>x>>y;
+
+    solution(x, y);
+}
+
+int main(){
+    input();
+    return 0;
+}
