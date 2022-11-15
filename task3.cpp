95ad7d050a9edb155e255813aa47e24fb206236d
starlght0 <112991120+starlght0@users.noreply.github.com>
2022-11-15 23:13:55 +0700

    Create task3.cpp

---
+++ task3.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+void output(string number){
+    cout<<number;
+}
+
+void solution(string number){
+
+    for(int i = 0;i < number.size();i++){
+        for(int j = i + 1;j < number.size();j++){
+            if(number[i] == number[j]){
+                output("true");
+                return;
+            }
+        }
+    }
+    output("false");
+}
+
+void input(){
+    string number;
+    cin>>number;
+
+    solution(number);
+}
+
+int main(){
+    input();
+    return 0;
+}
